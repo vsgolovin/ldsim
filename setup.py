d0606e6ef93645ec18e708f4c13c8534205bca33
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-07-17 17:43:00 +0300

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ldsim',
-    version='0.4.0',
+    version='0.4.1',
     author='Vyacheslav Golovin',
     description='Tools for modelling laser diode DC operation',
     packages=find_packages(exclude=['tests'],),
